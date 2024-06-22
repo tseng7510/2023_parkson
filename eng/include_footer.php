20a0421c1b3fcf56bea884a6cdb20f3eba5a53fe
Tseng <tseng7510@gmail.com>
2024-06-22 08:47:23 +0800

    更新

--- eng/include_footer.php
+++ eng/include_footer.php
@@ -1,7 +1,6 @@
 <footer class="footerBox wow">
   <div class="footer">
     <div class="container">
-
       <div class="footerLogo"><img src="../images/footer_logo.svg" alt=""></div>
 
       <div class="footerMenu">
@@ -14,6 +13,41 @@
           <li><a href="inquiry.php">Inquiry</a></li>
           <li><a href="disclaimer.php">USE & DISCLAIMER</a></li>
         </ul>
+
+        <div class="shareBox wow fadeInUp">
+          <ul>
+            <li>
+              <a href="#" target="_blank" class="facebook"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="youtube"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="linkedin"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="whatsapp"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="x"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="qq"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="instagram"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="skype"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="line"></a>
+            </li>
+            <li>
+              <a href="#" target="_blank" class="wechat"></a>
+            </li>
+          </ul>
+        </div>
         <div class="copyright">
           Copyright © PARKSON WU INDUSTRIAL CO., LTD., All Rights Reserved.
           <a href="https://www.letsmedia.tw/" target="_blank">Designed</a> by Lets Media <a href="https://www.ezb2b.com/" target="_blank">EZB2B</a><span><a href="https://www.letsmedia.tw/" target="_blank">網頁設計</a> ：雷斯媒體 EZB2B</span>
